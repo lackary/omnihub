a3d7f6e4303a3af3fded5d453f90b31eabe0cc9a
Yu-Chan (Henry) Huang <lackary@gmail.com>
2025-12-15 20:28:40 +0800

    feat(ui): use generated APP_VERSION in AccountScreen
    This commit replaces the hardcoded version string with the dynamically generated `APP_VERSION` from `BuildKonfig`.
    
    Changes include:
    - Imported `io.lackstudio.omnihub.BuildKonfig.APP_VERSION`.
    - Removed the local hardcoded `APP_VERSION` constant.

--- composeApp/src/commonMain/kotlin/io/lackstudio/omnihub/ui/account/AccountScreen.kt
+++ composeApp/src/commonMain/kotlin/io/lackstudio/omnihub/ui/account/AccountScreen.kt
@@ -19,10 +19,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import io.lackstudio.omnihub.BuildKonfig.APP_VERSION
 import io.lackstudio.omnihub.ui.navigation.Feature
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-private const val APP_VERSION= "0.1.0"
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
